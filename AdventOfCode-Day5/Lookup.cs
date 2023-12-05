b5c74bdf732f79845699c85b4227a7a76241087c
Ian Winter <ianskonto@googlemail.com>
2023-12-05 14:53:25 +0100

    Day5 Part1

---
+++ AdventOfCode-Day5/Lookup.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Immutable;
+
+namespace AdventOfCode_Day5;
+
+public class Lookup<TS,TD>(IImmutableList<LookupRule<TS,TD>> rules)
+{
+    public TD Map(TS source) => 
+        rules.First(r => r.Applicable(source)).Lookup(source);
+}
---
+++ AdventOfCode-Day5/LookupRule.cs
@@ -0,0 +1,8 @@
+﻿namespace AdventOfCode_Day5;
+
+public class LookupRule<TS, TD>(Func<TS,bool> canBeApplied, Func<TS,TD> lookup)
+{
+    public bool Applicable(TS source) => canBeApplied(source);
+
+    public TD Lookup(TS source) => lookup(source);
+}
