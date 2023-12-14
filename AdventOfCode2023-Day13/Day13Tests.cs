8054fe8dd4f3c40a1b99f824e6b97d4011916b0d
Ian Winter <ianskonto@googlemail.com>
2023-12-14 19:06:40 +0100

    Day13

--- AdventOfCode2023-Day13/Day13Tests.cs
+++ AdventOfCode2023-Day13/Day13Tests.cs
@@ -1,10 +1,77 @@
+using System.Collections.Immutable;
+
 namespace AdventOfCode2023_Day13;
 
 public class Day13Tests
 {
     [Fact]
-    public void Test1()
+    public void Example1_GetScore_405()
     {
+        var blocks = ToNoteBlocks(Example1.AsLines());
+            
+         var scoreSum = blocks.Select(nb => nb.GetScore()).Sum();
 
+         scoreSum.Should().Be(405);
     }
+
+    [Fact]
+    public void PuzzleInput_GetScore_36041()
+    {
+        var blocks = ToNoteBlocks(InputLoader.LoadLineByLine());
+
+        var scoreSum = blocks.Select(nb => nb.GetScore()).Sum();
+
+        scoreSum.Should().Be(36041);
+    }
+
+    [Fact]
+    public void Example1_GetSmudgedScore_400()
+    {
+        var blocks = ToNoteBlocks(Example1.AsLines());
+            
+        var scoreSum = blocks.Select(nb => nb.GetSmudgedScore()).Sum();
+
+        scoreSum.Should().Be(400);
+    }
+
+    [Fact]
+    public void PuzzleInput_GetSmudgedScore_35915()
+    {
+        var blocks = ToNoteBlocks(InputLoader.LoadLineByLine());
+
+        var scoreSum = blocks.Select(nb => nb.GetSmudgedScore()).Sum();
+
+        scoreSum.Should().Be(35915);
+    }
+
+    [Fact]
+    public void P_MirrorVerticallyPRef()
+    {
+        NoteBlock.MirrorVertically(new Point2D(2, 0), 5).Should().BeEquivalentTo(new Point2D(9, 0));
+        NoteBlock.MirrorVertically(new Point2D(3, 1), 5).Should().BeEquivalentTo(new Point2D(8, 1));
+    }
+
+    private static IImmutableList<NoteBlock> ToNoteBlocks(IImmutableList<string> allLines) 
+        => allLines
+            .SplitBy("")
+            .Select(lines => lines.CollectToString("\n"))
+            .Select(NoteBlock.From)
+            .ToImmutableList();
+
+    private static string Example1 => 
+@"#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#";
 }
---
+++ AdventOfCode2023-Day13/NoteBlock.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Immutable;
+
+namespace AdventOfCode2023_Day13;
+
+public sealed record NoteBlock(IImmutableDictionary<Point2D, char> Notes, int Width, int Height)
+{
+    public static NoteBlock From(string noteBlockText)
+    {
+        var lines = noteBlockText.AsLines();
+        return new NoteBlock(noteBlockText.CharsBy2dPosition(), lines[0].Length, lines.Count);
+    }
+
+    private IImmutableList<int> GetVerticalMirrors()
+    {
+        return Enumerable.Range(0, Width-1).Where(IsMirroredVerticallyAt).Select(i => i + 1).ToImmutableList();
+    }
+
+    private IImmutableList<int> GetHorizontalMirrors()
+    {
+        return Enumerable.Range(0, Height-1).Where(IsMirroredHorizontallyAt).Select(i => i + 1).ToImmutableList();
+    }
+
+    private bool IsMirroredHorizontallyAt(int y)
+    {
+        return Notes
+            .Where(t => t.Key.Y <= y)
+            .All(t => !Notes.TryGetValue(MirrorHorizontally(t.Key, y), out var refP) || refP == t.Value);
+    }
+
+    private bool IsMirroredVerticallyAt(int x)
+    {
+        return Notes
+            .Where(t => t.Key.X <= x)
+            .All(t => !Notes.TryGetValue(MirrorVertically(t.Key, x), out var refP) || refP == t.Value);
+    }
+
+    public long GetScore() => GetScore([], []);
+    private long GetScore(IEnumerable<int> exceptVerticals, IEnumerable<int> exceptHorizontals) => 
+        GetVerticalMirrors().Except(exceptVerticals).Sum() + GetHorizontalMirrors().Except(exceptHorizontals).Select(i => i * 100).Sum();
+
+    public static Point2D MirrorVertically(Point2D p, int x) => p with { X =  Math.Abs(p.X - x - 1) + x };
+    private static Point2D MirrorHorizontally(Point2D p, int y) => p with { Y =  Math.Abs(p.Y - y - 1) + y };
+
+    public long GetSmudgedScore()
+    {
+        return GetSmudges()
+            .Select(nb => nb.GetScore(GetVerticalMirrors(), GetHorizontalMirrors()))
+            .First(s => s > 0);
+    }
+
+    private IEnumerable<NoteBlock> GetSmudges()
+    {
+        foreach (var (p, c) in Notes)
+        {
+            yield return new NoteBlock(Notes.SetItem(p, c == '.' ? '#' : '.'), Width, Height);
+        }
+    }
+}
